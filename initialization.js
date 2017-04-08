9abdc656b3aac09d3dc26e252291652dfc878b80
gishmel <lukeagreenleaf@gmail.com>
2017-04-08 03:25:21 -0400

    Changed a couple of things around and removed things that will not be necessary for the new pipeline. It will be interesting what will be the easiest way to tie my blog directly to the application as it should probably be a subdomain of the larger website. Possibly in a side projects area of the blog?

--- initialization.js
+++ initialization.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // This mongo will be used as a selector inside of kubernetes and it will get the IP address of the running container that is labeled mongo
-const dbConnection = mongoose.connect("mongodb://mongo:27017/twitterSentimentsGiphy");
+const dbConnection = mongoose.connect("mongodb://96.249.210.132:3127/twitterSentimentsGiphy");
 mongoose.connection.on("error", console.error);
 
 
